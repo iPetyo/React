9e30e9ef5bd2751e5e53ac2c8db9dffd13fd79be
ipetyo <tu@email.competyoivov23@gmail.com>
2024-04-08 16:32:16 +0200

    guitarla-v2

---
+++ guitarla-v2/README.md
@@ -0,0 +1,27 @@
+# React + Vite Project
+
+This project is a web application developed using React and Vite, and a custom hook has been implemented for certain functionality.
+
+## Live Demo
+
+You can see a live demo of the project [here](https://6603f90e74273397ca0e01db--incandescent-melomakarona-504953.netlify.app/).
+
+## Technologies Used
+
+- **React**: JavaScript framework for building user interfaces.
+- **Vite**: Build tool providing a fast development environment for JavaScript and TypeScript projects.
+- **Custom Hook**: A custom hook has been implemented for specific project functionality.
+
+## Installation
+
+1. Clone this repository.
+2. Install dependencies using npm or yarn:
+
+## Contributing
+If you'd like to contribute to this project, please follow these steps:
+
+1. Fork the project.
+2. Create a new branch (git checkout -b feature/new-feature).
+3. Make your changes and commit them (git commit -am 'Add a new feature').
+4. Push your branch (git push origin feature/new-feature).
+5. Open a Pull Request.
---
+++ guitarla-v2/src/reducer/cart-reducer.ts
@@ -0,0 +1,111 @@
+import { db } from "../data/db"
+import { CartItem, Guitar } from "../types"
+
+export type CartActions =
+    { type: "add-to-cart", payload: { item: Guitar } } |
+    { type: "remove-from-cart", payload: { id: Guitar['id'] } } |
+    { type: "decrease-quantity", payload: { id: Guitar['id'] } } |
+    { type: "increase-quantity", payload: { id: Guitar['id'] } } |
+    { type: "clear-cart" }
+
+export type CartState = {
+    data: Guitar[],
+    cart: CartItem[]
+}
+
+const MIN_ITEMS = 1
+const MAX_ITEMS = 5
+
+const initialCart = () : CartItem[] => {
+    const localStorageCart = localStorage.getItem('cart')
+    return localStorageCart ? JSON.parse(localStorageCart) : []
+}
+
+export const initialState: CartState = {
+    data: db,
+    cart: initialCart()
+}
+
+export const cartReducer = (
+    state: CartState = initialState,
+    action: CartActions
+) => {
+
+    let itemExists;
+    let updatedCart: CartItem[] = []
+    switch (action.type) {
+        case "add-to-cart":
+
+            itemExists = state.cart.find(guitar => guitar.id === action.payload.item.id)
+
+            if (itemExists) {
+                updatedCart = state.cart.map(item => {
+                    if (item.id === action.payload.item.id) {
+                        if (item.quantity < MAX_ITEMS) {
+                            return { ...item, quantity: item.quantity + 1 }
+                        } else {
+                            return item
+                        }
+                    } else {
+                        return item
+                    }
+                })
+            } else {
+                const newItem: CartItem = { ...action.payload.item, quantity: 1 }
+                updatedCart = [...state.cart, newItem]
+            }
+            return {
+                ...state,
+                cart: updatedCart
+            }
+            break;
+        case "increase-quantity":
+            updatedCart = state.cart.map(item => {
+                if (item.id === action.payload.id && item.quantity < MAX_ITEMS) {
+                    return {
+                        ...item,
+                        quantity: item.quantity + 1
+                    }
+                }
+                return item
+            })
+            return {
+                ...state,
+                cart: updatedCart
+            }
+            break;
+
+        case "decrease-quantity":
+            updatedCart = state.cart.map(item => {
+                if (item.id === action.payload.id && item.quantity > MIN_ITEMS) {
+                    return {
+                        ...item,
+                        quantity: item.quantity - 1
+                    }
+                }
+                return item
+            })
+
+            return {
+                ...state,
+                cart: updatedCart
+            }
+            break;
+        case "remove-from-cart":
+            updatedCart = state.cart.filter(guitar => guitar.id !== action.payload.id)
+            return {
+                ...state,
+                cart: updatedCart
+            }
+
+            break;
+        case "clear-cart":
+            return {
+                ...state,
+                cart: []
+            }
+            break;
+        default:
+            break;
+    }
+}
