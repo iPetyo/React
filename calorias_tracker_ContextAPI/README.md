09ef5758576f7cb22cf3ff11675c5097c4d187c2
ipetyo <tu@email.competyoivov23@gmail.com>
2024-04-10 15:18:34 +0200

    calorias_tracker_ContextAPI

---
+++ calorias_tracker_ContextAPI/README.md
@@ -0,0 +1,29 @@
+# React + TypeScript + Vite
+
+This project is a web application developed using React and Vite. Tailwind CSS has been utilized for the interface design.
+
+## Live Demo
+
+You can see a live demo of the project [here](https://6613c8b62f17ff8837c01960--magenta-crostata-5288d4.netlify.app/).
+
+## Technologies Used
+
+- **React**: JavaScript framework for building user interfaces.
+- **Vite**: Build tool providing a fast development environment for JavaScript and TypeScript projects.
+- **Tailwind CSS**: Utility-first CSS framework for creating rapid and customizable designs.
+- **TypeScript**: Programming language that adds static types to JavaScript to improve code productivity and scalability.
+- **React Reducers:**: React reducers have been used for certain project functionality.
+
+## Installation
+
+1. Clone this repository.
+2. Install dependencies using npm or yarn
+
+## Contributing
+If you'd like to contribute to this project, please follow these steps:
+
+1. Fork the project.
+2. Create a new branch (git checkout -b feature/new-feature).
+3. Make your changes and commit them (git commit -am 'Add a new feature').
+4. Push your branch (git push origin feature/new-feature).
+5. Open a Pull Request.
---
+++ calorias_tracker_ContextAPI/src/components/Form.tsx
@@ -0,0 +1,105 @@
+import { useState, ChangeEvent, useEffect } from "react"
+import { v4 as uuidv4 } from 'uuid'
+import { Activity } from "../types"
+import { categories } from "../data/categories"
+import { useActivity } from "../hooks/useActivity"
+
+const inicalState: Activity = {
+    id: uuidv4(),
+    category: 1,
+    name: '',
+    calories: 0
+}
+
+export default function Form() {
+
+    const { state, dispatch } = useActivity();
+    const [activity, setActivity] = useState<Activity>(inicalState)
+
+    useEffect(() => {
+        if (state.activeId) {
+            const selectActivity = state.activities.filter(stateActivity => stateActivity.id === state.activeId)[0]
+            setActivity(selectActivity)
+        }
+    }, [state.activeId])
+
+    const handleChange = (e: ChangeEvent<HTMLInputElement> | ChangeEvent<HTMLSelectElement>) => {
+        const isNumberFiled = ['category', 'calories'].includes(e.target.id)
+
+        setActivity({
+            ...activity,
+            [e.target.id]: isNumberFiled ? +e.target.value : e.target.value
+        })
+    }
+
+    const isValidActivity = () => {
+        const { name, calories } = activity
+        return name.trim() !== '' && calories > 0
+    }
+
+    const handleSubmit = (e: ChangeEvent<HTMLFormElement>) => {
+        e.preventDefault();
+
+        dispatch({ type: "save-activity", payload: { newActivity: activity } })
+        //reiniciar form
+        setActivity({
+            ...inicalState,
+            id: uuidv4()
+        });
+    }
+
+    return (
+        <form
+            className="space-y-5 bg-white shadow p-10 rounded-lg"
+            onSubmit={handleSubmit}
+        >
+            <div className="grid grid-cols-1 gap-3">
+                <label htmlFor="category" className="font-bold">Categorias</label>
+                <select
+                    className="border border-slate-300 p-2 rounded-lg w-full bg-white"
+                    id="category"
+                    value={activity.category}
+                    onChange={handleChange}
+                >
+                    {categories.map(category => (
+                        <option
+                            key={category.id}
+                            value={category.id}
+                        >
+                            {category.name}
+                        </option>
+                    ))}
+                </select>
+            </div>
+            <div className="grid grid-cols-1 gap-3">
+                <label htmlFor="name" className="font-bold">Actividad:</label>
+                <input
+                    type="text"
+                    id="name"
+                    className="border border-slate-300 p-2 rounded-lg w-full bg-white"
+                    placeholder="Ej. Comida, Ensalada, Ejercico, Pesas, Bicicleta"
+                    value={activity.name}
+                    onChange={handleChange}
+                />
+            </div>
+            <div className="grid grid-cols-1 gap-3">
+                <label htmlFor="calories" className="font-bold">Calorias:</label>
+                <input
+                    type="number"
+                    id="calories"
+                    className="border border-slate-300 p-2 rounded-lg w-full bg-white"
+                    placeholder="Calorias. Ej 200"
+                    value={activity.calories}
+                    onChange={handleChange}
+                />
+            </div>
+
+            <input
+                type="submit"
+                className="bg-gray-800 hover:bg-gray-900 w-full p-2 font-bold uppercase text-white cursor-pointer disabled:opacity-10"
+                value={activity.category === 1 ? "Guardar comida" : "Guardar ejercicio"}
+                disabled={!isValidActivity()}
+            />
+        </form>
+    )
+}
